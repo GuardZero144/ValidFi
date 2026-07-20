81ab7eeb01c57ecbb67d6e62946f636404e29067
“rhemy-arc” <rhemaadzer@gmail.com>
2026-07-21 00:54:03 +0100

    project implementeation

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/inhousecodes/Documents/GuardZero/frontend/src/app/layout.tsx
+// File: /Users/inhousecodes/Documents/rehm-validfi/ValidFi/frontend/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- frontend/.next/types/app/page.ts
+++ frontend/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/inhousecodes/Documents/GuardZero/frontend/src/app/page.tsx
+// File: /Users/inhousecodes/Documents/rehm-validfi/ValidFi/frontend/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
